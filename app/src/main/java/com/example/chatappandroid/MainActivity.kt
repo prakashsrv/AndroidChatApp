b134488e14f1df4f0308e6c3e9aaa8e63af17568
Saurav <sauravp@Sauravs-MacBook-Neo.local>
2026-06-27 13:25:23 +0530

    feat: add ChatApplication and wire Hilt into MainActivity

--- app/src/main/java/com/example/chatappandroid/MainActivity.kt
+++ app/src/main/java/com/example/chatappandroid/MainActivity.kt
@@ -12,14 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatappandroid.ui.theme.ChatAppAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ChatAppAndroidTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
